d63ac9e7be74cfd0377b43bf7ce193722020f1e6
Tucker <th26tava@siena.edu>
2019-04-01 13:13:34 -0400

    Updated navbar function

--- functions.php
+++ functions.php
@@ -100,9 +100,9 @@ function make_bottom($javascript = null) {
   return '
       <!-- javascript -->
       <style>'.$javascript.'</style>
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
     </html>
   ';
@@ -170,18 +170,21 @@ function make_navbar() {
   return '
         <header style="background-color: #05386b">
           <!-- website navbar -->
-          <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #05386b">
-            <a class="navbar-brand" href="index.html" style="color: #edf5e1">Tucker Tavarone</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainnav" 
-                          aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon" style="background-color: #05386b"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="mainnav">
-              <div class="navbar-nav justify-content-end" >
-                '.$menu_item.'
-              </div>
-            </div>
-          </nav>
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Account Settings</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Login</a>
+                    <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Join</a>
+                    <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Users</a>
+                </div>
+            </li>
+          </ul>
         </header>';
   }
 
